d84c253fb390a033310f64c11d6bd84973d46521
jedika <jekjedika@gmail.com>
2021-04-30 08:21:01 +0300

    fix( remove arc)

--- src/components/dijkstra/useGraph.ts
+++ src/components/dijkstra/useGraph.ts
@@ -1,4 +1,5 @@
 import { computed, reactive } from '@vue/composition-api';
+import { cloneDeep } from 'lodash';
 import { Edge, Graph, Nodes, Options } from './useGraph.type';
 
 export const graph = reactive<Graph>({
@@ -10,7 +11,6 @@ export const graph = reactive<Graph>({
   nodeSize: 20,
   edgeSize: 4
 });
-
 
 export const useGraph = () => {
   const options = computed<Options>(() => {
@@ -26,27 +26,27 @@ export const useGraph = () => {
     };
   });
 
-  function initSelections(node = false, edge = false){
-    if(node) {
-      graph.selectedNodes = []
+  function initSelections(node = false, edge = false) {
+    if (node) {
+      graph.selectedNodes = [];
 
-    graph.nodes = graph.nodes.map(node => {
-      node._color = ''
-      return node
-    })
+      graph.nodes = graph.nodes.map(node => {
+        node._color = '';
+        return node;
+      });
     }
 
-    if(edge) {
+    if (edge) {
       graph.selectedEdge = null;
 
       graph.edges = graph.edges.map(edge => {
-        edge._color = ''
-        return edge
-      })
+        edge._color = '';
+        return edge;
+      });
     }
   }
 
-  function addNodes(){
+  function addNodes() {
     let id = 0;
     for (let i = 0; i < graph.nodes.length; i++) {
       ++id;
@@ -61,40 +61,44 @@ export const useGraph = () => {
     const length = graph.selectedNodes.length;
     const find = graph.selectedNodes.find(node => node.id == e.id) || null;
 
-    switch(true){
-      case (length == 0 ):
+    switch (true) {
+      case length == 0:
         graph.selectedNodes.push(e);
         break;
-        case (length == 1 && find == null):
-          const edgeIsExist = graph.edges.find(edge => (
-            (edge.sid == graph.selectedNodes[0].id && edge.tid == e.id) ||
-            (edge.tid == graph.selectedNodes[0].id && edge.sid == e.id))
+      case length == 1 && find == null:
+        const edgeIsExist =
+          graph.edges.find(
+            edge =>
+              (edge.sid == graph.selectedNodes[0].id && edge.tid == e.id) ||
+              (edge.tid == graph.selectedNodes[0].id && edge.sid == e.id)
           ) || null;
 
-          if(!edgeIsExist)graph.selectedNodes.push(e);
-          break;
-      case (length <= 2 && find != null):
-        graph.selectedNodes = graph.selectedNodes.filter(node => node.id != find?.id)
+        if (!edgeIsExist) graph.selectedNodes.push(e);
         break;
-      case (length == 2 && find == null):
-        graph.selectedNodes.pop()
-        graph.selectedNodes.push(e)
+      case length <= 2 && find != null:
+        graph.selectedNodes = graph.selectedNodes.filter(
+          node => node.id != find?.id
+        );
+        break;
+      case length == 2 && find == null:
+        graph.selectedNodes.pop();
+        graph.selectedNodes.push(e);
         break;
     }
 
     graph.nodes = graph.nodes.map(node => {
-      node._color = ''
-      return node
-    })
+      node._color = '';
+      return node;
+    });
 
     graph.selectedNodes.forEach(selectedNode => {
       graph.nodes = graph.nodes.map(node => {
-        if(node.id == selectedNode.id) {
+        if (node.id == selectedNode.id) {
           node._color = 'orange';
         }
-        return node
-      })
-    })
+        return node;
+      });
+    });
 
     // graph.selectedEdge = null;
 
@@ -103,22 +107,21 @@ export const useGraph = () => {
     //   return edge
     // })
 
-    initSelections(false, true)
-
+    initSelections(false, true);
   }
 
   function setSelectedEdge(e: Edge) {
     graph.selectedEdge = e;
 
     graph.edges = graph.edges.map(edge => {
-      edge._color = ''
-      return edge
-    })
+      edge._color = '';
+      return edge;
+    });
 
     graph.edges = graph.edges.map(edge => {
-      if(edge.id == e.id) edge._color = 'orange'
-      return edge
-    })
+      if (edge.id == e.id) edge._color = 'orange';
+      return edge;
+    });
 
     // graph.selectedNodes = []
 
@@ -127,72 +130,91 @@ export const useGraph = () => {
     //   return node
     // })
 
-    initSelections(true, false)
+    initSelections(true, false);
   }
-
 
   function addEdge(arcValue = 1) {
     const edgeLength = graph.edges.length;
 
-    if(graph.selectedNodes.length == 2){
+    if (graph.selectedNodes.length == 2) {
       graph.edges.push({
         id: edgeLength.toString(),
         sid: graph.selectedNodes[0].id,
         tid: graph.selectedNodes[1].id,
         name: arcValue,
         _color: ''
-      })
+      });
 
-    //   graph.selectedNodes = []
+      //   graph.selectedNodes = []
 
-    // graph.nodes = graph.nodes.map(node => {
-    //   node._color = ''
-    //   return node
-    // })
-    initSelections(true, true)
+      // graph.nodes = graph.nodes.map(node => {
+      //   node._color = ''
+      //   return node
+      // })
+      initSelections(true, true);
     }
-  };
+  }
 
   function updateAdge(name: number) {
-    if(graph.selectedEdge != null) {
+    if (graph.selectedEdge != null) {
       graph.edges.map(edge => {
-
-        if(edge.id == graph.selectedEdge?.id){
-          edge.name = name
+        if (edge.id == graph.selectedEdge?.id) {
+          edge.name = name;
         }
 
-        return edge
-      })
+        return edge;
+      });
 
-      initSelections(true, true)
+      initSelections(true, true);
     }
   }
 
   function removeEdge() {
-    if(graph.selectedEdge) {
-      graph.edges = graph.edges.filter(i => i.sid != graph.selectedEdge?.sid && i.tid != graph.selectedEdge?.tid)
+    if (graph.selectedEdge) {
+      const getEdge = graph.edges.filter(
+        i =>
+          i.sid == graph.selectedEdge?.sid && i.tid == graph.selectedEdge?.tid
+      )[0];
+
+      graph.edges = cloneDeep(graph.edges.filter(i => i !== getEdge));
     }
   }
 
-  const getLastNode = computed(() => graph.nodes[graph.nodes.length > 0 ? graph.nodes.length - 1 : graph.nodes.length ]
-    )
-
+  const getLastNode = computed(
+    () =>
+      graph.nodes[
+        graph.nodes.length > 0 ? graph.nodes.length - 1 : graph.nodes.length
+      ]
+  );
 
   function removeNode(): boolean {
     let isLinked = true;
 
-    if(getLastNode.value){
-        isLinked = graph.edges.filter(i => i.sid == getLastNode.value.id || i.tid == getLastNode.value.id).length > 0
-        console.log({isLinked})
-        if(!isLinked) {
-          // bola ts vita ##################
-          graph.nodes = graph.nodes.filter(n => n.id != getLastNode.value.id)
-        }
+    if (getLastNode.value) {
+      isLinked =
+        graph.edges.filter(
+          i => i.sid == getLastNode.value.id || i.tid == getLastNode.value.id
+        ).length > 0;
+      console.log({ isLinked });
+      if (!isLinked) {
+        // bola ts vita ##################
+        graph.nodes = graph.nodes.filter(n => n.id != getLastNode.value.id);
+      }
     }
 
     return !isLinked;
   }
 
-  return {options, graph, addNodes, setSelectedNode, addEdge, updateAdge, setSelectedEdge, removeEdge, removeNode, getLastNode}
-
-}
+  return {
+    options,
+    graph,
+    addNodes,
+    setSelectedNode,
+    addEdge,
+    updateAdge,
+    setSelectedEdge,
+    removeEdge,
+    removeNode,
+    getLastNode
+  };
+};
